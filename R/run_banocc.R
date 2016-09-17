f751137d3250a5bf327628e442ba89a661e2950e
Emma Schwager <emh146@mail.harvard.edu>
2016-09-17 15:07:13 -0400

    Changed example run_banocc to not tested instead of not run

--- R/run_banocc.R
+++ R/run_banocc.R
@@ -71,7 +71,8 @@
 #' }
 #' 
 #' @examples
-#' \dontrun{
+#' \donttest{
+#'   data(compositions_null)
 #'   compiled_banocc_model <- rstan::stan_model(model_code=banocc::banocc_model)
 #'   b_output <- run_banocc(C=compositions_null,
 #'                          compiled_banocc_model=compiled_banocc_model)
