c30617c96f5c98c98c9fc0c24bdcf4c17657539d
Emma Schwager <emh146@mail.harvard.edu>
2016-10-11 13:57:48 -0400

    Prettified model code

--- R/banocc_model.R
+++ R/banocc_model.R
@@ -13,21 +13,21 @@
 #' @export
 
 banocc_model <- "data {
-    int<lower=0> P; // number of elements of the composition
-    int<lower=0> N; // the number of subjects sampled
-    matrix<lower=0,upper=1>[N,P] C; // matrix of N comp. samples, P features
-    vector[P] n;  // the mean prior parameter for mu
-    cov_matrix[P] L; // the scale prior parameter for mu
-    real<lower=0> lambda; // the glasso prior parameter
+  int<lower=0> P; // number of elements of the composition
+  int<lower=0> N; // the number of subjects sampled
+  matrix<lower=0,upper=1>[N,P] C; // matrix of N comp. samples, P features
+  vector[P] n;  // the mean prior parameter for mu
+  cov_matrix[P] L; // the scale prior parameter for mu
+  real<lower=0> lambda; // the glasso prior parameter
 }
 
 parameters {
-    vector[P] m; // the lognormal centrality parameter
+  vector[P] m; // the lognormal centrality parameter
   cov_matrix[P] O; // the log-basis precision matrix
 }
 
-transformed parameters {
-  cov_matrix[P] S; // the log-basis covariance matrix
+transformed parameters{
+  cov_matrix[P] S;
   vector<lower=0>[P] s;
   corr_matrix[P] W;
 
@@ -37,35 +37,35 @@ transformed parameters {
 }
 
 model {
-    matrix[N,P] alpha_star;
-    real s_sq_star;
-    vector[N] m_star;
-    real inc_1;
-    real inc_2;
-    vector[N] inc_3i;
-    vector[N] inc_4i;
+  matrix[N,P] alpha_star;
+  real s_sq_star;
+  vector[N] m_star;
+  real inc_1;
+  real inc_2;
+  vector[N] inc_3i;
+  vector[N] inc_4i;
 
-    m ~ multi_normal(n, L);
-    for(k in 1:P){
-      O[k,k] ~ exponential(lambda/2);
-      for (i in (k+1):P){
-        O[i, k] ~ double_exponential(0, lambda);
-      }
+  m ~ multi_normal(n, L);
+  for(k in 1:P){
+    O[k,k] ~ exponential(lambda/2);
+    for (i in (k+1):P){
+      O[i, k] ~ double_exponential(0, lambda);
     }
+  }
 
   s_sq_star = inv(sum(O));
 
-    alpha_star   = rep_matrix(to_row_vector(m), N) - log(C);
+  alpha_star   = rep_matrix(to_row_vector(m), N) - log(C);
   m_star = rows_dot_product(alpha_star * O, rep_matrix(1, N, P)) * s_sq_star;
 
-    inc_1 = 0.5 * N * log(s_sq_star);
+  inc_1 = 0.5 * N * log(s_sq_star);
   inc_2 = 0.5 * N * log_determinant(O);
 
-    target += inc_1 + inc_2;
+  target += inc_1 + inc_2;
 
-    for (i in 1:N){
-        inc_4i[i] =  0.5 * m_star[i] * m_star[i] / s_sq_star;
-                  }
-    target += sum(inc_3i + inc_4i);
+  for (i in 1:N){
     inc_3i[i] =  - 0.5 * quad_form(O, alpha_star[i]' );
+    inc_4i[i] =  0.5 * m_star[i] * m_star[i] / s_sq_star;
+  }
+  target += sum(inc_3i + inc_4i);
 }"
